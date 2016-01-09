5976a0012bb6acde47e1870bd5854ba599a7073a
sudendradev@gmail.com <sudendradev@gmail.com>
2016-01-09 19:25:17 +0530

    show keyboard

--- app/src/main/java/bizapps/com/myparkz/utils/Utility.java
+++ app/src/main/java/bizapps/com/myparkz/utils/Utility.java
@@ -3,11 +3,21 @@ package bizapps.com.myparkz.utils;
 import android.app.Activity;
 import android.content.Context;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 /**
  * Created by sudendra.kamble on 09/01/16.
  */
 public class Utility {
+
+  public static void showKeyboard(Activity activity, EditText editText) {
+    try {
+      InputMethodManager imm =
+          (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+      imm.showSoftInput(editText, InputMethodManager.SHOW_FORCED);
+    } catch (Exception e) {
+    }
+  }
 
   public static void hideKeyboard(Activity context) {
     try {
